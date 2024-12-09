666c313b6adcc43850d2ecfadbc1ebd941338ca2
darsan012 <dsrsangautam@gmail.com>
2024-12-09 18:31:27 -0500

    Added /api/quiz/generate routes to the app, changes in load env aswell

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import dotenv from "dotenv";
-import { healthCheck } from './src/helpers/healthCheck.js';
+import 'dotenv/config'
 
-// to load the .env file
-dotenv.config();
+import { healthCheck } from './src/helpers/healthCheck.js';
+import quizRouter from './src/routes/quizRoutes.js'
 
 // created the express app
 const app = express();
 app.use(express.json());
 app.use('/api/healthCheck', healthCheck)
+app.use('/api/quiz', quizRouter);
+
 
 const port = process.env.PORT || 3000;
 
