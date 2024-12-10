87a629c2e25cf7ba9104020d58923c061cf8eb2b
darsan012 <dsrsangautam@gmail.com>
2024-12-09 23:52:52 -0500

    Added cors to allow cross origin access

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
+import cors from 'cors';
 
 import { healthCheck } from './src/helpers/healthCheck.js';
 import quizRouter from './src/routes/quizRoutes.js'
 
+
+const corsOptions = {
+    origin: [process.env.CLIENT_URL],
+    optionsSuccessStatus: 200,
+    credentials: true,
+  };
+
 // created the express app
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/api/healthCheck', healthCheck)
 app.use('/api/quiz', quizRouter);
 
